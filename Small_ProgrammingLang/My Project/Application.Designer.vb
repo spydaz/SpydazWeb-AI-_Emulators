0e8e4796b5dba61f5ffd744eedae14bf4d7a7c7b
Leroy Dyer <leroysamueldyer@hotmail.co.uk>
2021-05-06 12:21:49 +0100

    NewUI Multi Repl for Sal and Spl _ Both repls made in to TabControls For Master Proj

--- Small_ProgrammingLang/My Project/Application.Designer.vb
+++ Small_ProgrammingLang/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SDK.FrmPL_REPL
+            Me.MainForm = Global.SDK.Multi_REPL
         End Sub
     End Class
 End Namespace
